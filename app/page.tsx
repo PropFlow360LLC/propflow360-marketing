3aa80f5710424785054ddc31a169c987109ca2f1
PropFlow360LLC <propflow360@gmail.com>
2026-03-07 19:32:06 -0500

    Restore original marketing landing page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
 import Hero from "../components/hero"
-import PlatformPreview from "../components/platform-preview"
-import Features from "../components/features"
-import Pricing from "../components/pricing"
-import CTA from "../components/cta"
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <PlatformPreview />
-      <Features />
-      <Pricing />
-      <CTA />
     </>
   )
 }
